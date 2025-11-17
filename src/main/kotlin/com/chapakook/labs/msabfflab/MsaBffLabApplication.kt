a02460e1f0a669586e2b5492fde38192d38fd60a
chapakook <chapakook96@gmail.com>
2025-11-17 15:26:48 +0900

    initialize project structure with Gradle and Spring Boot setup

---
+++ src/main/kotlin/com/chapakook/labs/msabfflab/MsaBffLabApplication.kt
@@ -0,0 +1,11 @@
+package com.chapakook.labs.msabfflab
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsaBffLabApplication
+
+fun main(args: Array<String>) {
+    runApplication<MsaBffLabApplication>(*args)
+}
---
+++ src/test/kotlin/com/chapakook/labs/msabfflab/MsaBffLabApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.chapakook.labs.msabfflab
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsaBffLabApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
